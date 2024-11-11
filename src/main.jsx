b0dcd826a0bc193affa8e6dafe1945df8b8466ad
jhraju <jhraju890@gmail.com>
2024-11-11 10:04:05 +0600

    added banner site

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,9 @@ import router from './Routes/Routes';
 
  
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ <div className='container mx-auto'>
+   <StrictMode>
        <RouterProvider router={router} />
   </StrictMode>,
+ </div>
 )
